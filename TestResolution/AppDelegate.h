4454cbb4b1de9bfb2fe8d855e91d4858dba77634
Artem Abramov <artem@Artems-Mac-mini-2.local>
2015-02-04 13:09:17 +0300

    Initial Commit

---
+++ TestResolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestResolution
+//
+//  Created by Artem Abramov on 04/02/15.
+//  Copyright (c) 2015 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
